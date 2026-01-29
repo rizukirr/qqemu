9a6a9247a79aa84657a88bfbd51d0f4ee964f9c9
rizkirakasiwi <rizkirr.xyz@gmail.com>
2026-01-29 19:58:30 +0700

    fix: remote trailing after backslash

--- riscv64/run.sh
+++ riscv64/run.sh
@@ -19,7 +19,7 @@ qemu-system-riscv64 \
     -nographic \
     -cpu rv64 -smp cores=4 \
     -M virt -m 4096 \
-    -bios fw_payload.elf \ 
+    -bios fw_payload.elf \
     -drive if=none,format=qcow2,file="$QCOW2_IMAGE",id=hd0 \
     -device virtio-blk-device,drive=hd0 \
     -netdev user,id=net0,hostfwd=tcp::"$SSH_PORT"-:22 \
